7e5d21e59b4489097d6da15cf0029b1886fde987
jprihoda <jirkaprihoda@gmail.com>
2018-10-18 15:23:36 +0200

    broken commit

--- main.js
+++ main.js
@@ -13,80 +13,67 @@ window.Event = new Vue(); // event bus
 
 Vue.component('sidebar', {
   props: {
-    id: Number,
-    posts: Array
+    currentId: Number,
+    posts: Array,
+    end: false,
+    start: false,
 
   },
 
   data() {
     return {
-      currentIdUpdated: ""
+      currentIdUpdated: "",
     }
   },
 
   template:`
        <div class="sidebar">
         <div class="sidebar__navigation navigation">
-          <button class="navigation__next" @click="increaseId" >next</button>
-          <button class="navigation__previous" @click="decreaseId">prev</button>
+          <button class="navigation__next" @click="nextId" :disabled=start>next</button>
+          <button class="navigation__previous" @click="prevId" :disabled=end >prev</button>
           <button class="navigation__random" >rand</button>
           <button class="navigation__favourite">fav</button>
         </div>
       </div>
            `,
   methods: {
-      increaseId: function() {
-        if (this.id < this.posts.length){
-            Event.$emit('prev')
-        }else {
-          console.log("no newer comics")
-          Event.$emit('start');// add class deactivated to button
+      prevId: function() {
+        if (this.currentId < this.posts.length ){
+            Event.$emit('prev');
         }
       },
 
-      decreaseId: function() {
-        if (this.id > 1){
-          Event.$emit('next')
-        } else if (this.currentId = 1 ){
-          console.log("no older comics")
-          Event.$emit('end');// add class deactivated to button
+      nextId: function() {
+        if (this.currentId >= 1){
+          Event.$emit('next');
         }
       },
     },
 
     created() {
-      Event.$on('end', () => console.log("write disabled class"))
-      Event.$on('start', () => console.log("write disabled class"))
+      Event.$on('end', () => this.end = true)
+      Event.$on('start', () => this.start = true)
+
     }
 })
 
 Vue.component('post', {
     props: {
-      id: Number,
+      currentId: Number,
       title: String,
       src: String,
       tag: Array
     },
     template:`
         <div class="comicstrip">
-          <div class="comicstrip__title">{{ title }}{{ id }}</div>
+          <div class="comicstrip__title">{{ title }}{{ currentId }}</div>
           <div class="comicstrip__image"><img :src="src"></div>
           <div class="comicstrip__favorite"></div>
         </div>
         `,
 
-    data() {
-        return {
-            heading: 'TMSE'
-        }
-    },
-
     methods: {
 
-    },
-
-    created () {
-      Event
     }
 })
 
@@ -95,10 +82,12 @@ var app = new Vue({
     data: {
       heading: 'TMSE',
       currentId: 1,
+      end: false,
+      start: false,
       testVar: false,
       posts: [{
         'id': 3,
-        'title':'first post',
+        'title':'third post',
         'src': 'images/image-grey.png',
         'tag':["square","circle"]
         },
@@ -110,12 +99,13 @@ var app = new Vue({
         },
         {
           'id': 1,
-          'title':'third post',
+          'title':'first post',
           'src': 'images/image-green.jpg',
           'tag':["circle","hexagon", "square"]
         }
       ]
     },
+
     computed: {
       first () { return this.posts.slice(0, 1) },
 
@@ -123,20 +113,36 @@ var app = new Vue({
         return this.posts.find(post => {
           return post.id === this.currentId
         })
+      },
+
+      next() {
+        if (this.currentId === this.posts.length) {
+          console.log("no newer comics");
+          Event.$emit('start');// add class deactivated to button
+        } else {
+          this.currentId += 1;
+        }
       }
     },
+
     methods: {
-
-
-
+      // statecheck: function() {
+      //   if (this.currentId === 1) {
+      //     console.log("no older comics")
+      //     Event.$emit('end');// add class deactivated to button
+      //   } else if (this.currentId === this.posts.length){
+      //     console.log("no newer comics")
+      //     Event.$emit('start');// add class deactivated to button
+      //   }
+      // }
     },
 
     created () {
-      Event.$on('next', () =>
-        this.currentId -= 1);
+      Event.$on('next', next());
 
       Event.$on('prev', () =>
-        this.currentId += 1)
+        this.currentId -= 1);
+
     }
 })
 
